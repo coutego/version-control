317eee3118d617161941ec247f6905c1bd3897e1
Coutego <coutego@gmail.com>
2022-11-08 01:42:29 +0100

    add basic tests for DB

---
+++ tests/test_db.py
@@ -0,0 +1,36 @@
+import tempfile
+import shutil
+from unittest import TestCase
+from vc.prots import PObjectDB
+from vc.impl.db import DB
+from vc.impl.sha1hasher import SHA1Hasher
+from vc.impl.fs import create_vc_root_dir
+
+
+class DBTest(TestCase):
+    root: str
+    db: PObjectDB
+
+    def setUp(self):
+        h = SHA1Hasher()
+        self.root = tempfile.mkdtemp(dir=tempfile.gettempdir())
+
+        create_vc_root_dir(self.root)
+        self.db = DB(h, self.root)
+
+    def tearDown(self):
+        shutil.rmtree(self.root)
+
+    def test_not_found(self):
+        db = self.db
+        r = db.get("I dont exist")
+        self.assertEqual(r, None)
+        r = db.get(None)
+        self.assertEqual(r, None)
+
+    def test_basic_save(self):
+        db = self.db
+        c = "abc"
+        h = db.put(c)
+        ob = db.get(h).text
+        self.assertEqual(c, ob)
